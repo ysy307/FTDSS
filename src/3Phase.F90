b3f45d6e1461ecf7339f4af14de161b4fe217ab9
ysy307 <107449376+ysy307@users.noreply.github.com>
2025-07-04 07:43:02 +0000

    Deploying to gh-pages from @ ysy307/FTDSS@6f68df4b20bfb9040ecf788dfb9b56a50b9c9038 🚀

--- src/3Phase.F90
+++ src/3Phase.F90
@@ -1,32 +1,29 @@
-submodule(Calculate_VolumetricHeatCapacity) Calc_VHC_3
+submodule(Calculate_SpecificHeat) Calc_SPH_3
     implicit none
 contains
     !----------------------------------------------------------------------------------------------------
     ! Construct each type of density
     !----------------------------------------------------------------------------------------------------
-    module function VHC_3_Construct(iRegion, Input) result(Structure)
+    module function SPH_3_Construct(iRegion, Input) result(Structure)
         implicit none
-        class(Abst_VHC), allocatable :: Structure
+        class(Abst_SPH), allocatable :: Structure
         integer(int32), intent(in) :: iRegion
         type(Type_Input), intent(in) :: Input
 
         if (allocated(Structure)) deallocate (Structure)
-        allocate (Type_VHC_3Phase :: Structure)
+        allocate (Type_SPH_3Phase :: Structure)
 
-        Structure%Material1 = Input%Regions(iRegion)%Thermal%Cp(1)
-        Structure%Material2 = Input%Regions(iRegion)%Thermal%Cp(2)
-        Structure%Material3 = Input%Regions(iRegion)%Thermal%Cp(3)
+        Structure%Material1 = Input%Regions(iRegion)%Thermal%c(1)
+        Structure%Material2 = Input%Regions(iRegion)%Thermal%c(2)
+        Structure%Material3 = Input%Regions(iRegion)%Thermal%c(3)
 
-    end function VHC_3_Construct
+    end function SPH_3_Construct
 
-    module function Calc_VHC_GaussPoint_3Phase(self, state, DEN, LatentHeat, dQi_dT) result(VHC)
+    module function Calc_SPH_GaussPoint_3Phase(self, state) result(SPH)
         implicit none
-        class(Type_VHC_3Phase), intent(in) :: self
+        class(Type_SPH_3Phase), intent(in) :: self
         type(GaussPointState_t), intent(in) :: state
-        type(DENHolder), intent(in), optional :: DEN
-        real(real64), intent(in), optional :: LatentHeat
-        real(real64), intent(in), optional :: dQi_dT
-        real(real64) :: VHC
+        real(real64) :: SPH
 
         real(real64) :: phi1, phi2, phi3
 
@@ -34,7 +31,31 @@ contains
         phi2 = state%water_content
         phi3 = 1.0d0 - phi1 - phi2
 
-        VHC = Calc_VHC_3(self%Material1, phi1, self%Material2, phi2, self%Material3, phi3)
-    end function Calc_VHC_GaussPoint_3Phase
+        SPH = Calc_SPH_3(self%Material1, phi1, self%Material2, phi2, self%Material3, phi3)
+    end function Calc_SPH_GaussPoint_3Phase
 
-end submodule Calc_VHC_3
+    ! module subroutine Update_SPH_3(self, NodeBelonging, arr_phi1, arr_phi2, arr_phi3, arr_phi4)
+    !     implicit none
+    !     class(Type_SPH_3Phase), intent(inout) :: self
+    !     type(Belonging), intent(inout) :: NodeBelonging(:)
+    !     real(real64), intent(in), optional :: arr_phi1(:)
+    !     real(real64), intent(in), optional :: arr_phi2(:)
+    !     real(real64), intent(in), optional :: arr_phi3(:)
+    !     real(real64), intent(in), optional :: arr_phi4(:)
+    !     integer(int32) :: iN
+    !     real(real64) :: density_soil, density_water, density_ice
+
+    !     !$omp parallel do private(iN)
+    !     do iN = 1, self%nsize
+    !         self%value(iN, 1) = Calc_SPH_3(NodeBelonging(iN), &
+    !                                        self%soil, &
+    !                                        arr_phi1(iN), &
+    !                                        self%water, &
+    !                                        arr_phi2(iN), &
+    !                                        self%ice, &
+    !                                        arr_phi3(iN))
+    !     end do
+    !     !$omp end parallel do
+    ! end subroutine Update_SPH_3
+
+end submodule Calc_SPH_3
--- src/Base.F90
+++ src/Base.F90
@@ -1,59 +1,35 @@
-submodule(Calculate_VolumetricHeatCapacity) Calc_VHC_Base
+submodule(Calculate_SpecificHeat) Calc_SPH_Base
     implicit none
 
 contains
 
-    module subroutine VHCHolder_initialize(self, iRegion, Input)
+    module subroutine SPHHolder_initialize(self, iRegion, Input)
         implicit none
-        class(VHCHolder), intent(inout) :: self
+        class(SPHHolder), intent(inout) :: self
         integer(int32), intent(in) :: iRegion
         type(Type_Input), intent(in) :: Input
 
         if (Input%Regions(iRegion)%Flag%is3Phase) then
-            if (Input%Regions(iRegion)%Flag%isFrozen) then
-                self%c = Type_VHC_3Phase_Apparent(iRegion, input)
-            else
-                self%c = Type_VHC_3Phase(iRegion, Input)
-            end if
+            self%c = Type_SPH_3Phase(iRegion, Input)
         end if
 
-    end subroutine VHCHolder_initialize
+    end subroutine SPHHolder_initialize
 
-    module function Calc_VHC_3(VHC_soil, phi_soil, &
-                               VHC_water, phi_water, &
-                               VHC_ice, phi_ice) result(VHC)
+    module function Calc_SPH_3(SpecificHeat_soil, phi_soil, &
+                               SpecificHeat_water, phi_water, &
+                               SpecificHeat_ice, phi_ice) result(SpecificHeat)
         implicit none
-        real(real64), intent(in) :: VHC_soil
+        real(real64), intent(in) :: SpecificHeat_soil
         real(real64), intent(in) :: phi_soil
-        real(real64), intent(in) :: VHC_water
+        real(real64), intent(in) :: SpecificHeat_water
         real(real64), intent(in) :: phi_water
-        real(real64), intent(in) :: VHC_ice
+        real(real64), intent(in) :: SpecificHeat_ice
         real(real64), intent(in) :: phi_ice
-        real(real64) :: VHC
+        real(real64) :: SpecificHeat
 
-        VHC = VHC_soil * phi_soil &
-              + VHC_water * phi_water &
-              + VHC_ice * phi_ice
+        SpecificHeat = SpecificHeat_soil * phi_soil &
+                       + SpecificHeat_water * phi_water &
+                       + SpecificHeat_ice * phi_ice
 
-    end function Calc_VHC_3
-
-    module function Calc_VHC_3A(VHC_soil, phi_soil, VHC_water, phi_water, &
-                                VHC_ice, phi_ice, Lf, DEN_ice, dQi_dT) result(VHC)
-        implicit none
-        real(real64), intent(in) :: VHC_soil
-        real(real64), intent(in) :: phi_soil
-        real(real64), intent(in) :: VHC_water
-        real(real64), intent(in) :: phi_water
-        real(real64), intent(in) :: VHC_ice
-        real(real64), intent(in) :: phi_ice
-        real(real64), intent(in) :: Lf
-        real(real64), intent(in) :: DEN_ice
-        real(real64), intent(in) :: dQi_dT
-        real(real64) :: VHC
-
-        VHC = VHC_soil * phi_soil + VHC_water * phi_water + VHC_ice * phi_ice &
-              - Lf * DEN_ice * dQi_dT
-
-    end function Calc_VHC_3A
-
-end submodule Calc_VHC_Base
+    end function Calc_SPH_3
+end submodule Calc_SPH_Base
