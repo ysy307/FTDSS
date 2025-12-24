7b83a3f7c7a9a1be6b7c0422684798c046a3fafa
Kikuchi Shun <shungiku1012@gmail.com>
2025-12-24 16:51:40 +0900

    微修正

--- test/test_main.F90
+++ test/test_main.F90
@@ -29,38 +29,6 @@ contains
 
         call ftdss%initialize()
 
-        ! call ftdss%thermal
-
-        block
-            integer(int32), parameter :: num_steps = 100
-            integer(int32) :: i
-            real(real64) :: h_val, log_h
-            real(real64) :: val_theta
-            real(real64) :: h_values(num_steps)
-            real(real64) :: results_K(4, num_steps)
-            type(type_state) :: state
-            real(real64) :: res
-
-            integer(int32) :: unit
-
-            ! 計算ループ (h: -0.01 -> -10000)
-            open (newunit=unit, file="log/test/main_hcf.log", status="replace", action="write", iostat=ierr)
-            write (unit, '(A)') "# h,K_flh"
-            do i = 1, num_steps
-                ! ログスケールで h を生成 (-0.01 ~ -10000)
-                log_h = -2.0d0 + (6.0d0 * real(i - 1, real64) / real(num_steps - 1, real64))
-                h_val = -(10.0d0**log_h)
-
-                call state%reset()
-                call state%set(temperature=15.0d0, pressure=h_val)
-                ! WRF計算実行
-                call ftdss%thermal%physics%calc_Kflh(1, state, results_K(1, i))
-                write (unit, '(es24.15,",",es24.15)') h_val, results_K(1, i)
-                ! call wrf%p%calc(h_val, val_theta)
-            end do
-
-        end block
-
     end subroutine run_test_ftdss
 
 end program test_main
