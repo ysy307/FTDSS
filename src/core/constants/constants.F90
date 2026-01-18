0d09142e5fb2b30302a3e7b9f0cd70f539a44a51
Kikuchi Shun <shungiku1012@gmail.com>
2026-01-18 21:29:12 +0900

    Refactor core_constants_solver module by removing unused types and procedures
    - Eliminated type definitions and associated procedures for matrix types, physics types, nonlinear solvers, nonlinear norm criteria, and time units.
    - Removed unnecessary imports and cleaned up the module to enhance readability and maintainability.
    - Retained essential constants and parameters for solver status and convergence criteria.

--- src/core/constants/constants.F90
+++ src/core/constants/constants.F90
@@ -4,8 +4,9 @@ module core_constants
     use :: core_constants_parallel
     use :: core_constants_physical
     use :: core_constants_base
-    use :: core_constants_fe
+    ! use :: core_constants_fe
     use :: core_constants_ids
+    use :: core_constants_values
 
     implicit none
     public
