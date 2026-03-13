88fdc3a71c2ca13c6177ba39b422c8b46540adf5
Kikuchi Shun <shungiku1012@gmail.com>
2026-03-13 11:20:28 +0900

    Refactor setup_directory to simplify directory creation and file deletion logic

--- src/io/output/output_base.F90
+++ src/io/output/output_base.F90
@@ -1,43 +1,38 @@
 module io_output_base
     use, intrinsic :: iso_fortran_env, only: int32
-    use :: stdlib_strings, only:strip
+    use :: stdlib_strings, only: strip
     implicit none
     private
 
     public :: setup_directory
 
 contains
+
     subroutine setup_directory(dir_path, file_extensions)
         implicit none
         character(*), intent(in) :: dir_path
         character(*), intent(in) :: file_extensions(:)
 
         character(512) :: command
-        logical :: exists
         integer(int32) :: i
 
-        inquire (file=strip(dir_path), exist=exists)
-
-        if (.not. exists) then
 #ifdef _WIN32
-            command = "mkdir "//'"'//strip(dir_path)//'"'
+        command = "mkdir "//'"'//strip(dir_path)//'"'
+#else
+        command = "mkdir -p "//'"'//strip(dir_path)//'"'
 #endif
-#ifdef __linux__
-            command = "mkdir -p "//'"'//strip(dir_path)//'"'
+        call execute_safely(strip(command))
+
+        do i = 1, size(file_extensions)
+#ifdef _WIN32
+            command = "del /Q "//'"'//strip(dir_path)//"\*"//strip(file_extensions(i))//'"'
+#else
+            command = "rm -f "//'"'//strip(dir_path)//"*"//strip(file_extensions(i))//'"'
 #endif
             call execute_safely(strip(command))
-        else
-            do i = 1, size(file_extensions)
-#ifdef _WIN32
-                command = "del /Q "//'"'//strip(dir_path)//"*"//strip(file_extensions(i))//'"'
-#endif
-#ifdef __linux__
-                command = "rm -f "//strip(dir_path)//"*"//strip(file_extensions(i))
-#endif
-                call execute_safely(strip(command))
-            end do
-        end if
+        end do
     end subroutine setup_directory
+
 
     subroutine execute_safely(command)
         implicit none
@@ -46,8 +41,8 @@ contains
         character(256) :: cmd_msg
 
         exit_stat = 0
-        cmd_stat = 0
-        cmd_msg = ""
+        cmd_stat  = 0
+        cmd_msg   = ""
 
         call execute_command_line(command, wait=.true., exitstat=exit_stat, cmdstat=cmd_stat, cmdmsg=cmd_msg)
 
@@ -59,9 +54,9 @@ contains
             stop 1
         end if
 
-        ! Note: exit_stat represents the return code of the command itself.
-        ! Non-zero exit_stat is ignored here because commands like 'del'
-        ! may return non-zero when no files are found.
+        ! exit_stat is the return code of the command itself.
+        ! Non-zero values are allowed (e.g., rm when no files exist).
+
     end subroutine execute_safely
 
 end module io_output_base
